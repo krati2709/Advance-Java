5b87b060a74a8464a5305d4eb988a70565c4d5d5
krati2709 <kratitrivedi27@gmail.com>
2025-09-09 11:57:32 +0530

    new

--- JDBC-Project/src/com/rays/JDBC/TestConnection.java
+++ JDBC-Project/src/com/rays/JDBC/TestConnection.java
@@ -24,8 +24,8 @@ public class TestConnection {
 		
 		while(rs.next()) {
 			System.out.print(rs.getInt(1) + " ");
+			System.out.print("\t" +rs.getString(2) + " ");
 			System.out.println();
-			System.out.print(rs.getString(2) + " ");
 		}
 	}
 
---
+++ JDBC-Project/src/com/rays/JDBC/TestMarksheetMeritList.java
@@ -0,0 +1,26 @@
+package com.rays.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestMarksheetMeritList {
+	public static void main(String[] args) throws Exception {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
+		Statement stmt = conn.createStatement();
+		
+		ResultSet rs = stmt.executeQuery("select * , (physics + chemistry + maths + hindi + english) as total, "
+				+ "((physics + chemistry + maths + hindi + english)/5) as percentage from marksheet order by total desc limit 0,5;");
+		
+		while(rs.next()) {
+			System.out.print(rs.getInt(1) + " " + "\t" + rs.getString(2) + " " + "\t" + rs.getInt(3) + " " + "\t" + 
+					rs.getInt(4) + " " + "\t" + rs.getInt(5) + " " + "\t" + rs.getInt(6) + " " + "\t" +
+					rs.getInt(7) + " " + "\t" + rs.getInt(8) + " " + "\t" + rs.getDouble(9));
+			System.out.println();
+		}
+	}
+
+}
