56ff083c1577ac02c89c2df70c3f2587be07c963
krati2709 <kratitrivedi27@gmail.com>
2025-09-19 15:31:11 +0530

    done

--- JDBC-Project/src/com/rays/JDBC/preparedstatement/TestUserModel.java
+++ JDBC-Project/src/com/rays/JDBC/preparedstatement/TestUserModel.java
@@ -1,10 +1,27 @@
 package com.rays.jdbc.preparedstatement;
 
+import java.text.SimpleDateFormat;
+
 public class TestUserModel {
 	public static void main(String[] args) throws Exception {
 		
+		testAdd();
+		
+		
+	}
+	
+	public static void testAdd() throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		UserModel model = new UserModel();
-		System.out.println(model.nextPk());
+		UserBean bean = new UserBean();
+		
+		bean.setFirstName("Krati");
+		bean.setLastName("Trivedi");
+		bean.setLogin("krati27_");
+		bean.setPassword("kra123");
+		bean.setDob(sdf.parse("2002-09-27"));
+		model.add(bean);
 		
 	}
 
--- JDBC-Project/src/com/rays/JDBC/preparedstatement/UserModel.java
+++ JDBC-Project/src/com/rays/JDBC/preparedstatement/UserModel.java
@@ -21,5 +21,24 @@ public class UserModel {
 		conn.close();
 		return pk + 1;
 	}
+	
+	public void add(UserBean bean) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
+		PreparedStatement pstmt = conn.prepareStatement("insert into st_user values(?, ?, ?, ?, ?, ?)");
+		
+		int pk = nextPk();
+		
+		pstmt.setInt(1, pk);
+		pstmt.setString(2, bean.getFirstName());
+		pstmt.setString(3, bean.getLastName());
+		pstmt.setString(4, bean.getLogin());
+		pstmt.setString(5, bean.getPassword());
+		pstmt.setDate(6, new java.sql.Date(bean.getDob().getTime()));
+		
+		int i = pstmt.executeUpdate();
+		System.out.println("data inserted successfully: " + i);
+		conn.close();
+	}
 
 }
